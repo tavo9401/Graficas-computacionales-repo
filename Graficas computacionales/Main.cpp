9306facec53bdf69225452b57d8299fb24e4f996
solidtavo <A01375762>
2017-08-23 17:04:11 -0500

    Librerias configuradas

--- Graficas computacionales/Main.cpp
+++ Graficas computacionales/Main.cpp
@@ -5,16 +5,11 @@ Autor: A01375762 Octavio Adrian Valencia Ballesteros
 *********************************************************/
 
 #include <iostream>
-#include "Employee.h"
 using namespace std;
 
 int main()
 {
-	Employee empleado(1375762,"Octavio","Valencia",30000);
-	cout <<"Nombre del empleado: "<< empleado.GetName()<<endl;
-	cout << "Salario anual: " << empleado.GetAnnualSalary() << endl;
-	cout << "Se te ha aumentado a un salario de: " << empleado.RaiseSalary(50)<<endl<<endl;
-	cout << "Resumen: "<<empleado.Print();
+	cout << "Hola, mundo" << endl;
 	cin.get();
 	return 0;
 }
